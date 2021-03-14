9776087d1fee089689c5da1cc53c305cd38c4702
Thomas <thomastimmer11@hotmail.com>
2021-03-14 17:58:03 +0100

    chore: add env trait

--- src/environment.rs
+++ src/environment.rs
@@ -1,8 +1,18 @@
 use crate::statics::DEFAULT_VARIABLES;
-use crate::VariableMap;
+use crate::{VariableMap, ScopedVariables};
+
+pub trait Env<'a>{
+    fn variables(&self) -> &Box<dyn VariableMap + 'a>;
+    fn variables_mut(&mut self) -> &mut Box<dyn VariableMap + 'a>;
+}
 
 #[derive(Debug)]
 pub struct Environment<'a> {
+    variables: Box<dyn VariableMap + 'a>,
+}
+
+#[derive(Debug)]
+pub struct ScopedEnvironment<'a> {
     variables: Box<dyn VariableMap + 'a>,
 }
 
@@ -15,11 +25,33 @@ impl<'a> Environment<'a> {
     pub fn builder() -> EnvironmentBuilder<'a> {
         EnvironmentBuilder::default()
     }
+
+    pub fn to_scoped(&self) -> ScopedEnvironment {
+        Self::builder().with_variables(Box::new(ScopedVariables::new(self.variables()))).build_scoped()
+    }
+}
+
+impl<'a> Env<'a> for Environment<'a> {
+    fn variables(&self) -> &Box<dyn VariableMap + 'a>{
+        &self.variables
+    }
+    fn variables_mut(&mut self) -> &mut Box<dyn VariableMap + 'a>{
+        &mut self.variables
+    }
 }
 
 impl<'a> Default for Environment<'a> {
     fn default() -> Self {
         EnvironmentBuilder::default().build()
+    }
+}
+
+impl<'a> Env<'a> for ScopedEnvironment<'a> {
+    fn variables(&self) -> &Box<dyn VariableMap + 'a>{
+        &self.variables
+    }
+    fn variables_mut(&mut self) -> &mut Box<dyn VariableMap + 'a>{
+        &mut self.variables
     }
 }
 
@@ -30,6 +62,14 @@ impl<'a> EnvironmentBuilder<'a> {
             .unwrap_or(Box::new(DEFAULT_VARIABLES.to_owned()));
 
         Environment { variables }
+    }
+
+    pub fn build_scoped(self) -> ScopedEnvironment<'a> {
+        let variables = self
+            .variables
+            .unwrap_or(Box::new(DEFAULT_VARIABLES.to_owned()));
+
+        ScopedEnvironment { variables }
     }
 
     pub fn with_variables(
@@ -66,11 +106,11 @@ fn env_default_test() {
 
     assert_eq!(
         Some(&ExpressionValue::from(true)),
-        env.variables.get("true")
+        env.variables().get("true")
     );
     assert_eq!(
         Some(&ExpressionValue::from(true)),
-        expected.variables.get("true")
+        expected.variables().get("true")
     );
 }
 
@@ -96,5 +136,23 @@ fn build_test() {
         .with_variables(Box::new(vars))
         .build();
 
-    assert_eq!(Some(&ExpressionValue::from(1)), env.variables.get("test"));
+    assert_eq!(Some(&ExpressionValue::from(1)), env.variables().get("test"));
+}
+
+
+#[test]
+fn mutable_variables() {
+    use crate::ExpressionValue;
+    use std::collections::HashMap;
+
+    let mut vars = HashMap::new();
+    vars.insert(String::from("test"), 1.into());
+
+    let mut env = Environment::builder()
+        .with_variables(Box::new(vars))
+        .build();
+
+    let mut vars = env.variables_mut();
+
+    vars.insert("ok", 1.into());
 }
